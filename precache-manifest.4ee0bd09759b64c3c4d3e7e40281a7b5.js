0b53bab69b79f3db38c04f8dc4d4fb48372fa60a
Jack Antcio <anticoj@bc.edu>
2020-07-23 17:22:32 -0400

    Updates

---
+++ precache-manifest.4ee0bd09759b64c3c4d3e7e40281a7b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98d8b28016c3344335a01454f96bc09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "682a96db090b144d912c",
+    "url": "/static/css/main.7ccb2709.chunk.css"
+  },
+  {
+    "revision": "cbad801e49f2d92d66c2",
+    "url": "/static/js/2.8fba2040.chunk.js"
+  },
+  {
+    "revision": "16e72e8a7b17b4817847b2ca58cd85b3",
+    "url": "/static/js/2.8fba2040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682a96db090b144d912c",
+    "url": "/static/js/main.1d5dd348.chunk.js"
+  },
+  {
+    "revision": "b7564e67f0eb0dbfb612",
+    "url": "/static/js/runtime-main.52026b3d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quarantine-calculator/precache-manifest.5c2091e88f20259ea41a1ac663c3b0fa.js"
+  "/precache-manifest.4ee0bd09759b64c3c4d3e7e40281a7b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quarantine-calculator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
